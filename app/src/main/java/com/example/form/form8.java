e707073bf8ca4dc7a40d3291cea1a96d6e482ec2
LAN ANH <phamlananh670@gmail.com>
2023-10-31 17:28:41 +0700

    Final Commit

---
+++ app/src/main/java/com/example/form/form8.java
@@ -0,0 +1,20 @@
+package com.example.form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+
+public class form8 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form8);
+    }
+}
